ea3d062e26788fb3115d3520c5839a431f5cb37c
Patryk Laciak <matasinqwer@gmail.com>
2023-01-16 22:04:38 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Retool from 'react-retool';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <Retool url='https://onlyme.retool.com/embedded/public/4985be31-6687-48a3-83b5-2b4896171a8c' />
+    </div>
+  );
+}
+
+export default App;
