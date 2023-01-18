499fdd6b5f84429d8cf108a90080cc9ebd5c55e8
Patryk Laciak <matasinqwer@gmail.com>
2023-01-18 20:33:23 +0100

    feat: iframe

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import Retool from 'react-retool';
+
+import './App.css';
+
+function App() {
+  return (
+    // <div className='app'>
+    //   <Retool url='https://onlyme.retool.com/embedded/public/4985be31-6687-48a3-83b5-2b4896171a8c' />
+    // </div>
+    <div>
+      <iframe title='simple-katas-project' src='https://onlyme.retool.com/apps/Kudo%20app' width='100%' height='800px'></iframe>
+    </div>
+  );
+}
+
+export default App;
